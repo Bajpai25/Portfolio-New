564bb4a9dd28ed6dcac4992f6e2723a4191410df
Bajpai25 <shashwat.21250@knit.ac.in>
2024-01-11 23:18:48 +0530

    first-commit

---
+++ src/data/data.js
@@ -0,0 +1,76 @@
+ 
+
+export const data=[
+    {
+        id:1,
+        name:"Netflix Prototype",
+        image:"./project.jpg",
+        github:"https://github.com/Bajpai25/tasks",
+        live:"https://netflixclone-gamma-five.vercel.app/ ",
+    },
+    {
+        id:2,
+        name:"Talent Terrain",
+        image:"./project.jpg",
+        github:"https://github.com/Bajpai25/frontend",
+        live:"https://frontend-bajpai25.vercel.app/",
+    },
+    {
+        id:3,
+        name:"SparkScribe",
+        image:"./project.jpg",
+        github:"https://github.com/Bajpai25/SparkScribe",
+        live:"https://spark-scribe.vercel.app/",
+    },
+    {
+        id:4,
+        name:"Real Estate",
+        image:"./project.jpg",
+        github:"https://github.com/Bajpai25/Realestate",
+        live:"https://realestate-bajpai25.vercel.app/",
+    },
+    {
+        id:5,
+        name:"Chuckle Factory",
+        image:"./project.jpg",
+        github:"https://github.com/Bajpai25/tasks",
+        live:"https://task2-internship.vercel.app/ ",
+    },
+    {
+        id:6,
+        name:"Rescape",
+        image:"./project.jpg",
+        github:"https://github.com/Bajpai25/Rescape",
+        live:"https://rescape-sasuke.web.app",
+    },
+    {
+        id:7,
+        name:"Google Keep Clone",
+        image:"./project.jpg",
+        github:"https://github.com/Bajpai25/keep-clone",
+        live:"https://clone2--keep.web.app",
+    },
+    {
+        id:8,
+        name:"Weather App",
+        image:"./project.jpg",
+        github:"https://github.com/Bajpai25/Weather-App",
+        live:"https://weather-app-c4f5f.web.app",
+    },
+    {
+        id:9,
+        name:"Travel Website",
+        image:"./project.jpg",
+        github:"https://github.com/Bajpai25/Travel_",
+        live:"https://travel-bajpai25.vercel.app/",
+    },
+    {
+        id:10,
+        name:"Restaturant Website",
+        image:"./project.jpg",
+        gihub:"https://github.com/Bajpai25/restaurant-app",
+        live:"https://restaurant-site-c3367.web.app ",
+    }
+
+
+]
